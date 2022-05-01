3bdd8f5b27db7b561924ef6ce9adfd44a42d7ef8
kyle-makelog <kyle@makelog.io>
2022-05-01 13:12:46 -0600

    lint error

--- pages/qr.tsx
+++ pages/qr.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
 export default function QRPage() {
   return (
     <img
       src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fvanforsale.vercel.app%2F&chs=180x180&choe=UTF-8&chld=L|2"
-      rel="nofollow"
-      alt="qr code"
+      alt="QR Code"
+      width="100%"
     />
   );
 }
