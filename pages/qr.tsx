200c9b412244037fee4b2766f3994c3d996fcc48
kyle-makelog <kyle@makelog.io>
2022-05-01 13:08:23 -0600

    add qr code

---
+++ pages/qr.tsx
@@ -0,0 +1,9 @@
+export default function QRPage() {
+  return (
+    <img
+      src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fvanforsale.vercel.app%2F&chs=180x180&choe=UTF-8&chld=L|2"
+      rel="nofollow"
+      alt="qr code"
+    />
+  );
+}
