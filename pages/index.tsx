dea0517fdc2723f3f62279c983d34002d6ab4de9
kyle-makelog <kyle@makelog.io>
2022-05-14 16:58:43 -0600

    copy and style change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import type { NextPage } from "next";
 import { useEffect, useLayoutEffect, useState } from "react";
-import ReactBnbGallery from "react-bnb-gallery";
-import PhotoGrid from "../components/PhotoGrid";
-import styles from "../styles/IndexPage.module.css";
+import Image from "next/image";
 
 const IndexPage: NextPage = () => {
-  const [highlighted, highlight] = useState<string | undefined>();
-  const [photosSize, setPhotosSize] = useState(512);
-
   const photos = [
     "/images/8958B963-DCD1-4643-9BFE-C4F60E0BC1F1-1.jpg",
-    "/images/IMG_0211.jpeg",
+    //"/images/IMG_0211.jpeg",
     "/images/IMG_1098.jpeg",
     "/images/IMG_2071.jpeg",
     "/images/IMG_2352.jpeg",
@@ -21,91 +16,114 @@ const IndexPage: NextPage = () => {
     "/images/IMG_3879.jpeg",
     "/images/IMG_4277.jpeg",
     "/images/IMG_4279.jpeg",
-    "/images/IMG_4297.jpeg",
-    "/images/IMG_4493.jpeg",
-    "/images/IMG_8538.jpeg",
-    "/images/IMG_8836.jpeg",
-    "/images/IMG_8837.jpeg",
+    //"/images/IMG_4297.jpeg",
+    //"/images/IMG_4493.jpeg",
+    //"/images/IMG_8538.jpeg",
+    //"/images/IMG_8836.jpeg",
+    //"/images/IMG_8837.jpeg",
   ];
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    const handler = () => {
-      if (window.innerWidth > 768) {
-        setPhotosSize(window.innerWidth * 0.44 - 16 * 3);
+    const handle = setTimeout(() => {
+      if (index === photos.length - 1) {
+        setIndex(0);
       } else {
-        setPhotosSize(window.innerWidth - 16 * 3);
+        setIndex(index + 30);
       }
-    };
-
-    handler();
-    window.addEventListener("resize", handler);
+    }, 1000 * 1);
 
     return () => {
-      window.addEventListener("resize", handler);
+      clearInterval(handle);
     };
-  }, []);
+  }, [index, photos.length]);
 
   return (
-    <>
-      <header className={styles.header}>
-        <h1>For Sale 2019 Winnebago Revel, 61k miles, $159k</h1>
-      </header>
-      <main className={styles.columns}>
-        <aside className={styles.columnLeft}>
-          <PhotoGrid photos={photos} onClick={highlight} width={photosSize} />
-          {highlighted && (
-            <ReactBnbGallery
-              activePhotoIndex={photos.indexOf(highlighted)}
-              show
-              photos={photos}
-              onClose={() => highlight(undefined)}
-            />
-          )}
-        </aside>
-
-        <article className={styles.columnRight}>
-          <p className="larger">
-            Call or Text <a href="tel:(775)790-2797">(775)790-2797</a> for more
-            information
-          </p>
-
-          <dl>
-            <dt>General</dt>
-            <dd>Chassis and house in excellent condition</dd>
-            <dd>Last serviced by Mercedes April 2022</dd>
-            <dd>
-              Three brand new AGM batteries great for cold weather, installed
-              February 2022 with a 3-year warranty
-            </dd>
-
-            <dt>Upgrades and improvements</dt>
-            <dd>Custom mattress by Roam Rest</dd>
-            <dd>
-              Extra 28 gallon fuel tank installed by Agile Off-Road for a total
-              of 55 gallons of fuel
-            </dd>
-            <dd>Aluminess Rear Bumper</dd>
-            <dd>
-              Aluminess Swing Arms with Storage box, Spare Tire Holder, and Bike
-              Rack
-            </dd>
-            <dd>Aluminess Ladder</dd>
-            <dd>
-              Upgraded Espar heater only needs to be serviced every 5000 hours
-              vs. 1000 relocated for easier service
-            </dd>
-
-            <dt>Additional details</dt>
-            <dd>Have all maintenance and repair papers</dd>
-            <dd>
-              <a href="https://www.winnebago.com/Files/Images/Winnebago/brochures/2019/2019-revel-digital.x.pdf">
-                2019 Winnebago Revel Original Brochure
-              </a>
-            </dd>
-          </dl>
-        </article>
-      </main>
-    </>
+    <main>
+      <article>
+        <header>
+          <h1>
+            {`2019 Winnebago Revel Mercedes Sprinter 144'' 4x4 Adventure Van for Sale`}
+          </h1>
+        </header>
+        <p>
+          {`All the comforts of home while you explore your favorite spots.
+          Whether a weekend getaway to the mountains to beat the heat or a
+          months-long ski vacation, this Revel has everything you could need.
+          Get started with your adventure today.`}
+        </p>
+        <p>
+          {`Includes seatbelts for 4 and most comfortably sleeps 2 adults and 1 to
+          2 children. Includes wet bath (cassette toilet + shower),
+          refrigerator/freezer, sink, and induction burner. Take a look at the
+          original `}
+          <a href="https://www.winnebago.com/Files/Images/Winnebago/brochures/2019/2019-revel-digital.x.pdf">
+            brochure
+          </a>
+          {` to get all the details about what was included with the Revel.`}
+        </p>
+        <p>
+          {`We've also made a few upgrades, including:`}
+          <ul>
+            <li>
+              Added a 28-gallon auxiliary fuel tank to give you a 55-gallon
+              total capacity
+            </li>
+            <li>
+              Aluminess rear bumper with swing arms, storage box, spare tire
+              holder, and bike rack
+            </li>
+            <li>Aluminess Side Ladder</li>
+            <li>Upgraded Espar diesel heater</li>
+            <li>Custom mattress by Roam Rest </li>
+          </ul>
+          {`This van is in excellent condition inside and out. We've recently
+          installed 3 new AGM batteries for the house with a 3-year warranty.
+          All services on the chassis have been done on time, with the last
+          occurring in April of 2022. We are the original owners and will
+          provide all service records and repair papers. With just under `}
+          <b>62K miles</b>
+          {` and a fuel economy of `}
+          <b>17 MPG</b>
+          {` on the highway, the Sprinter
+          chassis will keep you going for many years without breaking the bank.`}
+        </p>
+        <p>
+          {`So why are we selling our Revel? We've been on the road in a van
+          for four years. The Revel is our third van, and while we love it, we
+          look forward to building a van that is better suited to our lifestyle
+          needs.`}
+        </p>
+        <p>
+          {`Asking for `}
+          <b>$159,000 OBO.</b>
+          {` The sale will be handled by Mercedes
+          Benz of St. George, Utah (No, you don't have to travel to Utah).
+          The Winnebago Revel is considered a Class B RV, so you may be eligible
+          for an RV loan. RV loans have longer terms than a standard vehicle
+          loan which would lower your monthly payment.`}
+        </p>
+        <p>
+          {`The van is currently in Mission Viejo, California. We will meet to
+          show in Orange County and immediate surrounding counties. (We may be
+          willing to travel for showings or delivery in or around California.)`}
+        </p>
+        <p>
+          {`Please call or text `}
+          <a href="tel:7755902797">(775)790-2797</a>
+          {` if interested.`}
+        </p>
+      </article>
+      <aside>
+        <Image
+          alt="Image"
+          src={photos[index]}
+          layout="fill"
+          objectFit="cover"
+          width="100%"
+        />
+      </aside>
+    </main>
   );
 };
 
