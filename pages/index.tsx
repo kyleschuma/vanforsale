2a51edab3f0f1df7ce7efff58e648171ca5eefd2
kyle-makelog <kyle@makelog.io>
2022-05-28 13:35:16 -0600

    copy changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -124,10 +124,7 @@ const IndexPage: NextPage = () => {
           </header>
 
           <p>
-            {`All the comforts of home while you explore your favorite spots.
-          Whether a weekend getaway to the mountains to beat the heat or a
-          months-long ski vacation, this Revel has everything you could need.
-          Get started with your adventure today.`}
+            {`All the comforts of home while you explore your favorite spots. Whether a weekend getaway to the mountains to beat the heat or a months-long ski vacation, this Revel has everything you could need. Get started with your adventure today.`}
           </p>
           <p>
             {`Includes seatbelts for 4 and most comfortably sleeps 2 adults and 1 to 2 children. Includes wet bath (cassette toilet + shower) that also doubles as a storage closet with removable bamboo shelving, refrigerator/freezer, sink, and induction burner. Take a look at the original `}
@@ -137,50 +134,36 @@ const IndexPage: NextPage = () => {
             {` to get all the details about what was included with the Revel.`}
           </p>
           <p>
-            {`We've also made a few upgrades, including:`}
+            {`Over $12,500 in upgrades, including:`}
             <ul>
               <li>
-                Added a 28-gallon auxiliary fuel tank to give you a 55-gallon
-                total capacity
+                28-gallon auxiliary fuel tank to give you a 55-gallon total
+                capacity
               </li>
               <li>
                 Aluminess rear bumper with swing arms, storage box, spare tire
-                holder, and bike rack
+                holder, bike rack, and Side Ladder
               </li>
-              <li>Aluminess Side Ladder</li>
               <li>Upgraded Espar diesel heater</li>
               <li>Custom mattress by Roam Rest </li>
             </ul>
-            {`This van is in excellent condition inside and out. We've recently
-          installed 3 new AGM batteries for the house with a 3-year warranty.
-          All services on the chassis have been done on time, with the last
-          occurring in April of 2022. We are the original owners and will
-          provide all service records and repair papers. With just under `}
+            {`This van is in excellent condition inside and out. We've recently installed 3 new AGM batteries for the house with a 3-year warranty. All services on the chassis have been done on time by Mercedes Benz, with the last occurring in April of 2022. A full house inspection was performed by El Toro RV in May of 2022. We are the original owners and will provide all service records and repair papers. With just under `}
             <b>62K miles</b>
             {` and a fuel economy of `}
             <b>17 MPG</b>
             {` on the highway, the Sprinter
-          chassis will keep you going for many years without breaking the bank.`}
+          chassis will keep you going for many years.`}
           </p>
           <p>
-            {`So why are we selling our Revel? We've been on the road in a van
-          for four years. The Revel is our third van, and while we love it, we
-          look forward to building a van that is better suited to our lifestyle
-          needs.`}
+            {`So why are we selling our Revel? We've been on the road in a van for four years. The Revel is our third van, and while we love it, we look forward to building a van that is better suited to our lifestyle needs.`}
           </p>
           <p>
             {`Asking for `}
             <b>$149,000 OBO.</b>
-            {` The sale will be handled by Mercedes
-          Benz of St. George, Utah (No, you don't have to travel to Utah).
-          The Winnebago Revel is considered a Class B RV, so you may be eligible
-          for an RV loan. RV loans have longer terms than a standard vehicle
-          loan which would lower your monthly payment.`}
+            {` The sale will be handled by Mercedes Benz of St. George, Utah (No, you don't have to travel to Utah). Trade-ins and financing may be available. The Winnebago Revel is considered a Class B RV, so you may be eligible for an RV loan. RV loans have longer terms than a standard vehicle  loan which would lower your monthly payment.`}
           </p>
           <p>
-            {`The van is currently in Mission Viejo, California. We will meet to
-          show in Orange County and immediate surrounding counties. (We may be
-          willing to travel for showings or delivery in or around California.)`}
+            {`The van is currently in Mission Viejo, California. We will meet to show in Orange County and immediate surrounding counties. (We may be willing to travel for showings or delivery in or around California.)`}
           </p>
           <p>
             {`Please call or text `}
